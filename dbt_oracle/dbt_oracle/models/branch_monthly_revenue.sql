01a0bcd4b8998773fe5ae71d19181179c69237a3
tanhtanh <akamelast123@gmail.com>
2024-10-16 08:41:13 +0700

    use dbt connect to oracle for query

---
+++ dbt_oracle/dbt_oracle/models/branch_monthly_revenue.sql
@@ -0,0 +1,23 @@
+
+WITH doanh_thu_cte AS (
+   SELECT
+    TO_CHAR(NGÀY_HẠCH_TOÁN, 'YYYY-MM') AS thang,
+    CHI_NHÁNH,
+    SUM(DOANH_THU) AS tong_doanh_thu
+FROM
+    "DỮ_LIỆU_BÁN_HÀNG"
+GROUP BY
+    TO_CHAR(NGÀY_HẠCH_TOÁN, 'YYYY-MM'),
+    CHI_NHÁNH
+ORDER BY
+    thang, CHI_NHÁNH
+)
+
+SELECT
+    thang,
+    CHI_NHÁNH,
+    tong_doanh_thu
+FROM
+    doanh_thu_cte
+ORDER BY
+    thang, CHI_NHÁNH
---
+++ dbt_oracle/dbt_oracle/target/compiled/DBT_ORACLE/models/personal_monthly_revenue.sql
@@ -0,0 +1,26 @@
+WITH doanh_thu_pe AS (
+    SELECT
+        TO_CHAR(DỮ_LIỆU_BÁN_HÀNG.NGÀY_HẠCH_TOÁN, 'YYYY-MM') AS thang,
+        DỮ_LIỆU_BÁN_HÀNG.MÃ_NHÂN_VIÊN_BÁN,
+        NHÂN_VIÊN.NHÂN_VIÊN_BÁN,
+        SUM(DỮ_LIỆU_BÁN_HÀNG.doanh_thu) AS tong_doanh_thu
+    FROM
+        "DỮ_LIỆU_BÁN_HÀNG" 
+    JOIN
+        "NHÂN_VIÊN" ON DỮ_LIỆU_BÁN_HÀNG.MÃ_NHÂN_VIÊN_BÁN = NHÂN_VIÊN.MÃ_NHÂN_VIÊN_BÁN
+    GROUP BY
+        TO_CHAR(DỮ_LIỆU_BÁN_HÀNG.NGÀY_HẠCH_TOÁN, 'YYYY-MM'),
+        DỮ_LIỆU_BÁN_HÀNG.MÃ_NHÂN_VIÊN_BÁN,
+        NHÂN_VIÊN.NHÂN_VIÊN_BÁN
+    ORDER BY
+        thang, NHÂN_VIÊN.NHÂN_VIÊN_BÁN
+)
+SELECT
+    thang,
+    MÃ_NHÂN_VIÊN_BÁN,
+    NHÂN_VIÊN_BÁN,
+    tong_doanh_thu
+FROM
+    doanh_thu_pe
+ORDER BY
+    thang, NHÂN_VIÊN_BÁN
